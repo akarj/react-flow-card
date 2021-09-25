c38d1cdfb15e03f8d0acb7109db249c967274cf8
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-09-25 20:06:52 +0530

    Image node working finished!!✔✔

--- src/Components/Modules/ImageField/ImageField.jsx
+++ src/Components/Modules/ImageField/ImageField.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import { Handle } from "react-flow-renderer";
 import "./ImageField.scss";
 
 export default function ImageField() {
-  return <div>image 🚷</div>;
+  const [imageFile, setImageFile] = useState(null);
+
+  // [Functions]
+  const changeHandler = event =>
+    setImageFile(URL.createObjectURL(event.target.files[0]));
+  return (
+    <div className="imageField-container">
+      <div className="title">
+        <header>
+          <h2>Image Field</h2>
+        </header>
+      </div>
+      <div className="image-container">
+        {imageFile && <img src={imageFile}></img>}
+      </div>
+      <div className="image-add">
+        <input
+          type="file"
+          name="imageFile"
+          id="imageFile"
+          accept="image/*"
+          placeholder="Add Image"
+          onChange={changeHandler}
+        />
+      </div>
+      <Handle className="nodeOut" type="source" position="right" />
+    </div>
+  );
 }
