c880d6b360262bc3e684f1e35e3b087db3a1c0da
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-09-25 20:39:39 +0530

    text Field JS working Finished!!✔✔

--- src/Components/Modules/TextField/TextField.jsx
+++ src/Components/Modules/TextField/TextField.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
 import "./TextField.scss";
 
 export default function TextField() {
-  return <div>text Filed💣 </div>;
+  const [text, setText] = useState("");
+  const changeHandler = event => {
+    setText(event.target.value);
+  };
+  return (
+    <div className="textField-container">
+      <div className="desc">
+        <header>
+          <h2>Text Field</h2>
+        </header>
+      </div>
+      <div className="text-show-field">{text && <span>{text}</span>}</div>
+      <div className="text-input-field">
+        <input
+          type="text"
+          name="text"
+          id="text"
+          max="1000"
+          onChange={changeHandler}
+        />
+      </div>
+    </div>
+  );
 }
